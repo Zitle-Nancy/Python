d61beabc999f3659865a15d1fa6146c7b84e4d89
Nancy Zitle <ancizj@hotmail.com>
2019-06-20 21:43:06 -0500

    Agrega if,else y valida con los operadores logicos como or y and

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ clientes = 'pablo, ricardo,'
 
 def create_client(client_name):
     global clientes
-    clientes += client_name
-    _add_comma()
+    if client_name not in clientes:
+        clientes += client_name
+        _add_comma()
+    else:
+        print('Client alredy is in the client\'s list')
 
 def _add_comma():
     global clientes
@@ -14,12 +17,25 @@ def list_clients():
     global clientes
     print(clientes)
 
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today')
+    print('[C]reate client')
+    print('[D]elete client')
 
 if __name__ == '__main__':
     list_clients()
-
-    create_client('yo')
-		
-    list_clients()
+    _print_welcome()
+    command = input()
+    if command == 'c' or command == 'C':
+        client_name = input('What is the cient name?')
+        create_client(client_name)
+        list_clients()
+    elif command == 'd' or command == 'D':
+        pass
+    else:
+        print('El comando es invalido')
+    
 
 
