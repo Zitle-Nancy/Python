349a6dc6ce0888afb9de871a43abadcb7f523fca
Nancy Zitle <ancizj@hotmail.com>
2019-07-02 11:26:25 -0500

    Create the function update_client

--- main.py
+++ main.py
@@ -13,27 +13,48 @@ def _add_comma():
     global clientes
     clientes +=','
 
+def get_client_name():
+    return input('What is the cient name? ')
+
 def list_clients():
     global clientes
     print(clientes)
 
+def update_client(client_name, update_client_name):
+    global clientes
+    if client_name in clientes:
+        clientes = clientes.replace(client_name + ',', update_client_name + ',')
+    else:
+        print('Client is not in clients list')
+    
+
+
 def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
     print('*' * 50)
-    print('What would you like to do today')
+    print('What would you like to do today? ')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
 
 if __name__ == '__main__':
     list_clients()
     _print_welcome()
+
     command = input()
+    command = command.upper()
+
     if command == 'c' or command == 'C':
-        client_name = input('What is the cient name?')
+        client_name = get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'd' or command == 'D':
         pass
+    elif command == 'U':
+        client_name = get_client_name()
+        update_client_name = input('What is the update client name? ')
+        update_client(client_name, update_client_name)
+        list_clients()
     else:
         print('El comando es invalido')
     
